d041f3a297228660bfc7854babf9de4b1d8e9068
sachindadwal <90467353+sachindadwal@users.noreply.github.com>
2021-09-16 10:09:25 +0000

    ex03: welcome your user

---
+++ index.js
@@ -0,0 +1,5 @@
+var readlineSync = require('readline-sync');
+//var readlineSync = require('readline-sync');
+
+var userName= readlineSync.question('What is your name:');
+console.log("Welcome to JavaScript World !! "+ userName)
